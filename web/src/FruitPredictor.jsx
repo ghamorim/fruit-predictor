60162e43c7eacf6fc5c9768b4ba8fac85de9cf29
ghamorim <gabriel.amorim@estudante.ufla.br>
2021-12-28 03:04:27 -0300

    feat: Adiciona página web principal

---
+++ web/src/FruitPredictor.jsx
@@ -0,0 +1,79 @@
+import React, { useState, useRef } from 'react';
+
+import api from './services/api';
+
+import './FruitPredictor.css';
+
+import fruitsIcon from './assets/fruits-icon.png';
+
+function FruitPredictor() {
+  const [photoUrl, setPhotoUrl] = useState(null);
+  const [prediction, setPrediction] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const hiddenFileInput = useRef(null);
+
+  const handleClick = (e) => {
+    hiddenFileInput.current.click();
+  }
+
+  const handleChange = async (e) => {
+    setPhotoUrl(URL.createObjectURL(e.target.files[0]));
+
+    const image = e.target.files[0];
+
+    const data = new FormData();
+    data.append('image', image);
+
+    setLoading(true);
+
+    api.post('/predict', data)
+      .then(response => setPrediction(response.data.predicted_class))
+      .finally(() => setLoading(false));
+  };
+
+
+  return (
+    <div className="container">
+      <div className="content">
+        <header className="title">
+          <h1>Fruit Predictor</h1>
+        </header>
+        <main>
+          <div className="panel-container">
+            <div className="panel">
+              { photoUrl ? (
+                <img className="uploaded-image" src={photoUrl} alt="Uploaded Image" />
+              ) : (
+                <img className="placeholder-image" src={fruitsIcon} alt="Placeholder" />
+              )}
+            </div>
+            { loading ? (
+              <p>
+                Predicting...
+              </p>
+            ) : (prediction && (
+              <p>
+                It's <b>{prediction}</b>!
+              </p>
+            ))}
+          </div>
+          <div className="options-container">
+            <span>Upload a photo:</span>
+            <input
+              accept=".png, .jpg, .jpeg, .bmp, .gif"
+              type="file"
+              ref={hiddenFileInput}
+              onChange={handleChange}
+            />
+            <button className="upload-button" onClick={handleClick}>
+              Upload
+            </button>
+          </div>
+        </main>
+      </div>      
+    </div>
+  );
+}
+
+export default FruitPredictor;
